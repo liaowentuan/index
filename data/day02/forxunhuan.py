1029c18c24dec86499cb91b5b58c1ceb2dfdf7cf
liaowentuan <996002654@qq.com>
2018-03-21 14:21:41 +0800

    change

--- data/day02/forxunhuan.py
+++
@@ -1,4 +0,0 @@
-arr = [1,2,3,4,5,6,77,8,9,10]
-print(arr)
-for item in arr[::2]:
-  print(item)
--- data/day02/osItem.py
+++
@@ -1,5 +0,0 @@
-import os
-localUrl = os.path.abspath("./")
-print(localUrl)
-print(os.path.exists("os.py"))
-print(os.path.exists("osItem.py"))
