b0005568b863efd0fbab6db92c60c33eeec2cfbc
liaowentuan <996002654@qq.com>
2018-03-22 14:22:27 +0800

    index

--- pythonData/day02/tupleItem.py
+++
@@ -1,3 +0,0 @@
-tup = (1,2,3,4)
-print(tup[2])
-print(tup.count(1))
--- src/store/mutations.js
+++
@@ -1,9 +0,0 @@
-const mutations = {
-  addition: state => state.count++,
-  minus: state => state.count--,
-  setZero: state => {
-    state.count = 0
-  }
-}
-
-export default mutations
