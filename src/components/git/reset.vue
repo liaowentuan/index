c80498e358590eb9bf4d8665b27860a6d2f1e2f4
liaowentuan <996002654@qq.com>
2018-03-27 22:52:34 +0800

    add

---
+++ src/components/git/reset.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page">
+        <h3>版本回退上一次提交</h3>
+        <pre>
+          git reset --hard HEAD^
+        </pre>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'reset'
+}
+</script>
+
+<style scoped>
+
+</style>
