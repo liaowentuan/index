8be2c072d85460279fe73dc470ee21a7d4b7827a
liaowentuan <996002654@qq.com>
2018-04-08 23:11:25 +0800

    django

--- src/components/Django/DjangoInstall.vue
+++ src/components/Django/DjangoInstall.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="page">
-        <h3>Django教程还需看 <a href="https://code.ziqiangxuetang.com/django/django-tutorial.html">《自强学堂》</a>的比较好</h3>
+      <h3>Django教程还需看 <a href="https://code.ziqiangxuetang.com/django/django-tutorial.html">《自强学堂》</a>的比较好</h3>
+      <pre>
+        pip install Django==1.11.12
+        django-admin startproject  项目名称  # 虚拟环境中运行最好
+      </pre>
     </div>
 </template>
 
