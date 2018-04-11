7f70965ae0f7ec2b95ae712b65f3ad8247cfc6c8
liaowentuan <996002654@qq.com>
2018-04-11 11:46:42 +0800

    flask

--- pythonData/day01/flaskDemo.py
+++ pythonData/day01/flaskDemo.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'hello world'
+def hello():
+    return {"param":request.args.get('abc')}
 
-@app.route('/login/', methods = ['GET','POST'])
+@app.route('/login/{x}', methods = ['GET','POST'])
 def login():
     if request.method == 'GET':
         # if request.form['username'] == request.form['password']:
@@ -15,6 +15,17 @@ def login():
         # else:
 #当form中的两个字段内容不一致时，返回我们所需要的测试信息
         return str(request.url)        #需要替换的部分
+
+@app.errorhandler(404)
+def page_not_found(error):
+    arr = {error, request.url}
+    string = """
+    <pre style="font-size:20px;">
+      %s,
+      %s
+    </pre>
+    """%(str(request.url), str(error))
+    return string, 404
 
 if __name__ == '__main__':
     # app.run(debug=True)
