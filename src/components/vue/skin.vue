e815002f1a0cb358d5918b848f12a981d16c35a5
liaowentuan <996002654@qq.com>
2018-04-11 22:05:36 +0800

    skin

---
+++ src/components/vue/skin.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="page">
+      <h3>
+        换肤
+      </h3>
+      <pre>
+        使用less编译器
+      </pre>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'skin'
+}
+</script>
+
+<style scoped>
+
+</style>
