a43dbe82eaed2432e559ec9e71101e24d3c43c13
liaowentuan <996002654@qq.com>
2019-06-11 20:46:45 +0800

    index

--- src/components/login.vue
+++ src/components/login.vue
@@ -25,36 +25,36 @@ export default {
   },
   methods: {
     login () {
-      this.$http({
-        method: 'POST',
-        url: '/controller/login/user/admin',
-        data: {
-          user_name: this.user,
-          u_passwd: this.upwd
-        }
-      }).then(function (res) {
-        if (res.data.status === true) {
-          window.sessionStorage.setItem('user', JSON.stringify(res.data))
-          window.location.href = '#/home'
-        }
-      }).catch(res => {
-        if (this.user === 'web') {
-          window.sessionStorage.setItem('ROLE', 'web')
-          this.$router.push({path: '/home/vue'})
-        } else if (this.user === 'python') {
-          window.sessionStorage.setItem('ROLE', 'python')
-          this.$router.push({path: '/home/python'})
-        } else if (this.user === 'superAdmin') {
-          window.sessionStorage.setItem('ROLE', 'superAdmin')
-          this.$router.push({path: '/home/vue'})
-        } else {
-          this.$notify({
-            title: '提示',
-            message: '您的输入有误, 请用 "web", "python", "superAdmin" 三个账号, 密码随意填写',
-            type: 'warning'
-          })
-        }
-      })
+      // this.$http({
+      //   method: 'POST',
+      //   url: '/controller/login/user/admin',
+      //   data: {
+      //     user_name: this.user,
+      //     u_passwd: this.upwd
+      //   }
+      // }).then(function (res) {
+      //   if (res.data.status === true) {
+      //     window.sessionStorage.setItem('user', JSON.stringify(res.data))
+      //     window.location.href = '#/home'
+      //   }
+      // }).catch(res => {
+      // })
+      if (this.user === 'web') {
+        window.sessionStorage.setItem('ROLE', 'web')
+        this.$router.push({path: '/home/vue'})
+      } else if (this.user === 'python') {
+        window.sessionStorage.setItem('ROLE', 'python')
+        this.$router.push({path: '/home/python'})
+      } else if (this.user === 'superAdmin') {
+        window.sessionStorage.setItem('ROLE', 'superAdmin')
+        this.$router.push({path: '/home/vue'})
+      } else {
+        this.$notify({
+          title: '提示',
+          message: '您的输入有误, 请用 "web", "python", "superAdmin" 三个账号, 密码随意填写',
+          type: 'warning'
+        })
+      }
     }
   }
 }
